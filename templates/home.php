6edda758435799ee292a1e02587901c3aa72887f
Duże WW <geny@example.com>
2020-04-11 19:32:42 +0200

    Add priv_menu.php with Private menu.

--- templates/home.php
+++ templates/home.php
@@ -1,23 +1,24 @@
 <?php $title = 'Private part. List of Posts' ?>
 
 <?php ob_start() ?>
-		<h1>Private part</h1>
-		<a href="profile.php">Profile</a>
-		<a href="logout.php">Logout</a>
 
-		<h2>Home Page</h2>
-		<p>Welcome back, <?=$_SESSION['name']?>!</p>
+  <h1>Private part</h1>
+  <?php include 'priv_menu.php' ?>
 
-    <h1>List of Posts</h1>
-    <ul>
-        <?php foreach ($posts as $post): ?>
-        <li>
-            <a href="/flatphp/show.php?id=<?= $post['id'] ?>">
+  <h2>Home Page</h2>
+  <p>Welcome back, <?=$_SESSION['name']?>!</p>
+
+  <h1>List of Posts</h1>
+  <ul>
+	<?php foreach ($posts as $post): ?>
+	 <li>
+		<a href="/flatphp/show.php?id=<?= $post['id'] ?>">
                 <?= $post['title'] ?>
-            </a>
-        </li>
-        <?php endforeach ?>
-    </ul>
+		</a>
+	 </li>
+	<?php endforeach ?>
+  </ul>
+
 <?php $content = ob_get_clean() ?>
 
 <?php include 'layout.php' ?>
